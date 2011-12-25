0010096cb48c18a18bb11bb0821162b148cc7365
Duncan Stuart <dgmstuart@gmail.com>
2011-12-25 20:20:06 +0000

    Refactoring to DRY up code

--- latlong.rb
+++ latlong.rb
@@ -10,34 +10,20 @@ class LatLong
  
   def to_xy(input)
     @input = input
-    result = signed_decimal_degrees
-    result = decimal_degrees_direction if result.nil? 
-    
-    result = signed_decimal_degrees_minutes if result.nil?
-    result = decimal_degrees_minutes_direction if result.nil?
-    
-    result = decimal_degrees_minutes_seconds_direction if result.nil?
-    result = signed_decimal_degrees_minutes_seconds if result.nil?
+    result = decimal_degrees
+    result = decimal_degrees_minutes if result.nil?
+    result = decimal_degrees_minutes_seconds if result.nil?
     
     sign_coordinates
   end
   
-  def signed_decimal_degrees
-    matches = /^(#{Sign})(#{UnsignedDecimal}) (#{Sign})(#{UnsignedDecimal})$/.match(@input)
-    unless matches.nil?
-      @y_sign, y, @x_sign, x = matches[1,4]
-      
-      @x=x.to_f
-      @y=y.to_f
-    end
-  end
-  
-  def decimal_degrees_direction
+  def decimal_degrees
     matches = /^(#{UnsignedDecimal}) (#{Direction}) (#{UnsignedDecimal}) (#{Direction})$/.match(@input)
     unless matches.nil?
       y, @y_sign, x, @x_sign = matches[1,4]
     else
-      matches = /^(#{Direction}) (#{UnsignedDecimal}) (#{Direction}) (#{UnsignedDecimal})$/.match(@input)
+      matches = /^(#{Sign})(#{UnsignedDecimal}) (#{Sign})(#{UnsignedDecimal})$/.match(@input)
+      matches = /^(#{Direction}) (#{UnsignedDecimal}) (#{Direction}) (#{UnsignedDecimal})$/.match(@input) if matches.nil?
       unless matches.nil?
         @y_sign, y, @x_sign, x  = matches[1,4]
       else
@@ -49,12 +35,13 @@ class LatLong
     @y=y.to_f
   end
   
-  def decimal_degrees_minutes_direction
+  def decimal_degrees_minutes
     matches = /^(#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Direction}) (#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Direction})$/.match(@input)
     unless matches.nil?
       y_degrees, y_minutes, @y_sign, x_degrees, x_minutes, @x_sign = matches[1,6]
     else  
       matches = /^(#{Direction}) (#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Direction}) (#{UnsignedInteger}) (#{UnsignedDecimal})$/.match(@input)
+      matches = /^(#{Sign})(#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Sign})(#{UnsignedInteger}) (#{UnsignedDecimal})$/.match(@input) if matches.nil?
       unless matches.nil?
         @y_sign, y_degrees, y_minutes, @x_sign, x_degrees, x_minutes = matches[1,6]
       else
@@ -62,25 +49,16 @@ class LatLong
       end
     end
     
-    get_coords_from_degrees_and_minutes(y_degrees, y_minutes, x_degrees, x_minutes)
+    get_coords_from_degrees_minutes_and_seconds(x_degrees, y_degrees, x_minutes, y_minutes)
   end
   
-  def signed_decimal_degrees_minutes
-    matches = /^(#{Sign})(#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Sign})(#{UnsignedInteger}) (#{UnsignedDecimal})$/.match(@input)
-    unless matches.nil?
-      @y_sign, y_degrees, y_minutes, @x_sign, x_degrees, x_minutes = matches[1,6]
-
-      get_coords_from_degrees_and_minutes(y_degrees, y_minutes, x_degrees, x_minutes)
-    end
-  end
-  
-  
-  def decimal_degrees_minutes_seconds_direction
+  def decimal_degrees_minutes_seconds
     matches = /^(#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Direction}) (#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Direction})$/.match(@input)
     unless matches.nil?
       y_degrees, y_minutes, y_seconds, @y_sign, x_degrees, x_minutes, x_seconds, @x_sign = matches[1,8]
     else  
       matches = /^(#{Direction}) (#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Direction}) (#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal})$/.match(@input)
+      matches = /^(#{Sign})(#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Sign})(#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal})$/.match(@input) if matches.nil?
       unless matches.nil?
         @y_sign, y_degrees, y_minutes, y_seconds, @x_sign, x_degrees, x_minutes, x_seconds = matches[1,8]
       else
@@ -88,26 +66,10 @@ class LatLong
       end
     end
     
-    get_coords_from_degrees_minutes_and_seconds(y_degrees, y_minutes, y_seconds, x_degrees, x_minutes, x_seconds)
+    get_coords_from_degrees_minutes_and_seconds(x_degrees, y_degrees, x_minutes, y_minutes, x_seconds, y_seconds)
   end
   
-  
-  def signed_decimal_degrees_minutes_seconds
-    matches = /^(#{Sign})(#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal}) (#{Sign})(#{UnsignedInteger}) (#{UnsignedInteger}) (#{UnsignedDecimal})$/.match(@input)
-    unless matches.nil?
-      @y_sign, y_degrees, y_minutes, y_seconds, @x_sign, x_degrees, x_minutes, x_seconds = matches[1,8]
-  
-      get_coords_from_degrees_minutes_and_seconds(y_degrees, y_minutes, y_seconds, x_degrees, x_minutes, x_seconds)
-    end
-  end
-  
-  
-  def get_coords_from_degrees_and_minutes(y_degrees, y_minutes, x_degrees, x_minutes)
-    @y = y_degrees.to_i + y_minutes.to_f/60
-    @x = x_degrees.to_i + x_minutes.to_f/60
-  end
-  
-  def get_coords_from_degrees_minutes_and_seconds(y_degrees, y_minutes, y_seconds, x_degrees, x_minutes, x_seconds)
+  def get_coords_from_degrees_minutes_and_seconds(x_degrees, y_degrees, x_minutes, y_minutes, x_seconds=0, y_seconds=0)
     @y = y_degrees.to_i + y_minutes.to_f/60 + y_seconds.to_f/3600
     @x = x_degrees.to_i + x_minutes.to_f/60 + x_seconds.to_f/3600
   end
