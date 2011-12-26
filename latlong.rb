42f764858673b4a29bcdd07e78f01dc1c3d41940
Duncan Stuart <dgmstuart@gmail.com>
2011-12-26 23:50:35 +0000

    refactored - not sure how much of an improvement it is

--- latlong.rb
+++ latlong.rb
@@ -3,60 +3,54 @@ class LatLong
   Decimal = "[0-9]*\.?[0-9]+"
   Integer ="[0-9]*"
   
-  Sign = "[-+]?"
-  Direction = "[NSEW]"
-  
- 
-  def to_xy(input)
-    @input = input
-    
-    matches = /^(#{Decimal}) (#{Direction}) (#{Decimal}) (#{Direction})$/.match(@input)
-    unless matches.nil?
-      y_degrees, y_sign, x_degrees, x_sign = matches[1,4]
+  def method_missing(meth)
+    method_name = meth.id2name
+    if method_name =~ /^([xy])_degrees$/
+      "(?<#{$1}_degrees>#{Decimal})"
+    elsif method_name =~ /^([xy])_degrees_minutes$/
+      "(?<#{$1}_degrees>#{Integer}) (?<#{$1}_minutes>#{Decimal})" 
+    elsif method_name =~ /^([xy])_degrees_minutes_seconds$/
+      "(?<#{$1}_degrees>#{Integer}) (?<#{$1}_minutes>#{Integer}) (?<#{$1}_seconds>#{Decimal})"
+    elsif method_name =~ /^([xy])_sign$/
+      "(?<#{$1}_sign>[-+]?)"
+    elsif method_name =~ /^([xy])_direction$/
+      "(?<#{$1}_sign>[NSEW])"
     else
-      matches = /^(#{Sign})(#{Decimal}) (#{Sign})(#{Decimal})$/.match(@input)
-      matches = /^(#{Direction}) (#{Decimal}) (#{Direction}) (#{Decimal})$/.match(@input) if matches.nil?
-      unless matches.nil?
-        y_sign, y_degrees, x_sign, x_degrees  = matches[1,4]
-      else
-        matches = /^(#{Integer}) (#{Decimal}) (#{Direction}) (#{Integer}) (#{Decimal}) (#{Direction})$/.match(@input)
-        unless matches.nil?
-          y_degrees, y_minutes, y_sign, x_degrees, x_minutes, x_sign = matches[1,6]
-        else  
-          matches = /^(#{Direction}) (#{Integer}) (#{Decimal}) (#{Direction}) (#{Integer}) (#{Decimal})$/.match(@input)
-          matches = /^(#{Sign})(#{Integer}) (#{Decimal}) (#{Sign})(#{Integer}) (#{Decimal})$/.match(@input) if matches.nil?
-          unless matches.nil?
-            y_sign, y_degrees, y_minutes, x_sign, x_degrees, x_minutes = matches[1,6]
-          else
-            matches = /^(#{Integer}) (#{Integer}) (#{Decimal}) (#{Direction}) (#{Integer}) (#{Integer}) (#{Decimal}) (#{Direction})$/.match(@input)
-            unless matches.nil?
-              y_degrees, y_minutes, y_seconds, y_sign, x_degrees, x_minutes, x_seconds, x_sign = matches[1,8]
-            else  
-              matches = /^(#{Direction}) (#{Integer}) (#{Integer}) (#{Decimal}) (#{Direction}) (#{Integer}) (#{Integer}) (#{Decimal})$/.match(@input)
-              matches = /^(#{Sign})(#{Integer}) (#{Integer}) (#{Decimal}) (#{Sign})(#{Integer}) (#{Integer}) (#{Decimal})$/.match(@input) if matches.nil?
-              unless matches.nil?
-                y_sign, y_degrees, y_minutes, y_seconds, x_sign, x_degrees, x_minutes, x_seconds = matches[1,8]
-              else
-                return #no match
-              end
-            end
-          end
-        end
-      end
+      raise NoMethodError, "undefined method `#{method_name}' for #{self}:#{self.class}"
     end
+  end
+  
+  def to_xy(input)
+    y=0
+    x=0
     
-    get_coords_from_degrees_minutes_and_seconds(x_degrees, y_degrees, x_minutes, y_minutes, x_seconds, y_seconds)
+    matches = /^#{y_degrees_minutes_seconds} #{y_direction} #{x_degrees_minutes_seconds} #{x_direction}$/.match(input)
+    matches = /^#{y_direction} #{y_degrees_minutes_seconds} #{x_direction} #{x_degrees_minutes_seconds}$/.match(input) if matches.nil?
+    matches = /^#{y_sign}#{y_degrees_minutes_seconds} #{x_sign}#{x_degrees_minutes_seconds}$/.match(input) if matches.nil?
+    
+    y += matches[:y_seconds].to_f/3600 unless matches.nil?
+    x += matches[:x_seconds].to_f/3600 unless matches.nil?
+    
+    matches = /^#{y_degrees_minutes} #{y_direction} #{x_degrees_minutes} #{x_direction}$/.match(input) if matches.nil?
+    matches = /^#{y_direction} #{y_degrees_minutes} #{x_direction} #{x_degrees_minutes}$/.match(input) if matches.nil?
+    matches = /^#{y_sign}#{y_degrees_minutes} #{x_sign}#{x_degrees_minutes}$/.match(input) if matches.nil?
+    
+    y += matches[:y_minutes].to_f/60 unless matches.nil?
+    x += matches[:x_minutes].to_f/60 unless matches.nil?
+    
+    matches = /^#{y_degrees} #{y_direction} #{x_degrees} #{x_direction}$/.match(input) if matches.nil?
+    matches = /^#{y_direction} #{y_degrees} #{x_direction} #{x_degrees}$/.match(input) if matches.nil?
+    matches = /^#{y_sign}#{y_degrees} #{x_sign}#{x_degrees}$/.match(input) if matches.nil?
+    
+    return if matches.nil?
+
+    y += matches[:y_degrees].to_f
+    x += matches[:x_degrees].to_f
     
     # North and East are positive in Cartesian space, West and South are negative  
-    @y = 0-@y if y_sign == "-" or y_sign == "S" 
-    @x = 0-@x if x_sign == "-" or x_sign == "W"
-    
-    [@x, @y]
-  end
-  
-  def get_coords_from_degrees_minutes_and_seconds(x_degrees, y_degrees, x_minutes=0, y_minutes=0, x_seconds=0, y_seconds=0)
-    @y = y_degrees.to_f + y_minutes.to_f/60 + y_seconds.to_f/3600
-    @x = x_degrees.to_f + x_minutes.to_f/60 + x_seconds.to_f/3600
-  end
+    y = 0-y if matches[:y_sign] == "-" or matches[:y_sign] == "S" 
+    x = 0-x if matches[:x_sign] == "-" or matches[:x_sign] == "W"
 
+    [x, y]
+  end
 end
